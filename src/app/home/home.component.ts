2c90f770bc3f1f3cd8e8815fe0ed7856e5663fe6
khanim4 <imroz786>
2018-10-01 11:58:19 -0400

    Fixing store interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,8 +3,8 @@ import {STATES} from '../models/app.constants';
 import {FormBuilder, FormGroup, Validators, FormControl, AbstractControl} from '@angular/forms';
 import {CONSTITUENCIES, Constituency} from '../models/constituencies';
 import { Store } from '@ngrx/store';
-import {AppData} from '../models/app.constants';
-import * as HomeActions from './../actions/home.actions'
+import {HomeState, AppData} from '../models/app.constants';
+import * as HomeActions from './../actions/home.actions';
 
 
 @Component({
@@ -14,8 +14,8 @@ import * as HomeActions from './../actions/home.actions'
 })
 export class HomeComponent implements OnInit {
   public states = STATES;
-  public profileForm : FormGroup;
-  public stateControl : AbstractControl;
+  public profileForm: FormGroup;
+  public stateControl: AbstractControl;
   public constituencyControl: AbstractControl;
   public ministerControl: AbstractControl;
   public constituencies = CONSTITUENCIES;
@@ -24,17 +24,18 @@ export class HomeComponent implements OnInit {
   public ministerSelected: string;
   public constituencySelected;
   public provinceSelected: string;
-  homeState;
-  initialState?: AppData;
+  private homeState;
+  private initialState?: AppData;
+  private noop = () => {};
 
-  constructor(private fb: FormBuilder, private store: Store<AppData>) {
+  constructor(private fb: FormBuilder, private store: Store<HomeState>) {
     this.homeState = store.select('homeState');
     this.homeState.subscribe(val => {
       this.initialState = val;
-    })
+    });
   }
 
-  setInitialValues(){
+  setInitialValues() {
     this.stateControl.setValue(this.initialState.provinceState);
     this.constituencyControl.setValue(this.initialState.constituencyState);
     this.ministerControl.setValue(this.initialState.ministerState);
@@ -55,58 +56,62 @@ export class HomeComponent implements OnInit {
     this.constituencyControl = this.profileForm.controls['constituency'];
     this.ministerControl = this.profileForm.controls['minister'];
     this.subscribeToValueChanges();
-    this.initialState ? this.setInitialValues(): '';
+    if (this.initialState) {
+      this.setInitialValues();
+    }
   }
 
-  subscribeToValueChanges(){
+  subscribeToValueChanges() {
     this.listenToValueChange();
     this.updateMinisterOnValueChange();
     this.updateMinisterSelected();
   }
 
-  listenToValueChange(){
+  listenToValueChange() {
     this.stateControl.valueChanges.subscribe(stateSelected => {
-      this.initialState && this.initialState !== stateSelected ? this.setToDefault() : '';
+      if (this.initialState && this.initialState !== stateSelected) {
+        this.setToDefault();
+      }
       this.provinceSelected = stateSelected;
       this.showParticularConstituency(stateSelected);
-    })
+    });
   }
 
-  setToDefault(){
+  setToDefault() {
     this.ministers = null;
     this.ministerSelected = null;
     this.constituencyControl.setValue('');
   }
 
-  showParticularConstituency(stateSelected: string){
+  showParticularConstituency(stateSelected: string) {
     this.stateConstituencies = this.constituencies[stateSelected];
   }
 
-  updateMinisterOnValueChange(){
+  updateMinisterOnValueChange() {
     this.constituencyControl.valueChanges.subscribe(constituencySelected => {
-      if(constituencySelected){
-        let constituencySelectedForMinisters = this.getParticularConstituency(constituencySelected);
+      if (constituencySelected) {
+        const constituencySelectedForMinisters = this.getParticularConstituency(constituencySelected);
         this.ministers = constituencySelectedForMinisters ? constituencySelectedForMinisters.ministers : null;
         this.constituencySelected = constituencySelectedForMinisters;
         this.setState(false);
       }
-    })
+    });
   }
 
-  updateMinisterSelected(){
+  updateMinisterSelected() {
     this.ministerControl.valueChanges.subscribe(minister => {
       this.ministerSelected = minister;
       this.setState();
-    })
+    });
   }
 
-  setState(updateMinister = true){
+  setState(updateMinister = true) {
     this.store.dispatch(new HomeActions.AddProvince(this.provinceSelected));
-    this.constituencySelected ? this.store.dispatch(new HomeActions.AddConstituency(this.constituencySelected.code)) : '';
-    updateMinister ? this.store.dispatch(new HomeActions.AddMinister(this.ministerSelected)) : '';
+    this.constituencySelected ? this.store.dispatch(new HomeActions.AddConstituency(this.constituencySelected.code)) : this.noop();
+    updateMinister ? this.store.dispatch(new HomeActions.AddMinister(this.ministerSelected)) : this.noop();
   }
 
-  getParticularConstituency(code): Constituency{
+  getParticularConstituency(code): Constituency {
     return this.stateConstituencies.find(item => item.code === code);
   }
 
