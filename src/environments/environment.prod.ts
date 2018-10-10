8f4dec88412dfeffc56c2b1184a39219d7f961b7
Imroz <imrozkhn7@gmail.com>
2018-10-10 10:36:29 -0400

    Updating production environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'AIzaSyBFSJlZFmeeleZe2bNq78Q-4xEFKAPTBis',
+  apiEndPoints: {
+    geoCoding: 'https://maps.googleapis.com/maps/api/geocode/json',
+    geoCodingMock: 'https://maps.googleapis.com/maps/api/geocode/json?address=Toledo&region=es&key=YOUR_API_KEY',
+    googleSearch: 'https://www.googleapis.com/customsearch/v1?key=',
+    cxCode: '004456939783387997491:877yxvo9iuu'
+  }
 };
